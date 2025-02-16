11f85f2bdba45a60a344b9af7d3fe677b519b4db
Kleyciane Dutra Mendes <160146713+MajorKley@users.noreply.github.com>
2025-02-15 21:34:45 -0300

    Add files via upload

---
+++ praticando-logica/alugames/js/app.js
@@ -0,0 +1,25 @@
+function alterarStatus(Id){
+    let gameClicado = document.getElementById(`game-${Id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+
+
+  if(imagem.classList.contains('dashboard__item__img--rented')){
+    if(confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = ('Alugar');
+    }
+  }else{
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.classList.add('dashboard__item__button--return');
+    botao.textContent = ('Devolver');
+   
+  }
+
+
+}
+
+
